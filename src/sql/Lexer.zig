2f3458332b38f5276968767169e26fec13da1738
Gino Heyman <gino.heyman@gmail.com>
2024-06-18 20:25:44 +0200

    feat: add SQL parser

---
+++ src/sql/Lexer.zig
@@ -0,0 +1,191 @@
+const std = @import("std");
+
+pub fn get_tokens(input: []const u8) TokenIterator {
+    return TokenIterator{ .input = input };
+}
+
+pub const TokenIterator = struct {
+    const Self = @This();
+
+    input: []const u8,
+    i: usize = 0,
+
+    /// Returns the next token until the input is exhausted.
+    pub fn next(self: *Self) ?Token {
+        const char = self.readChar();
+
+        if (char) |c| {
+            return switch (c) {
+
+                // operators
+                '-' => if (self.readMatchingChar('-')) |_| .{ .comment = self.readCommentLine() } else .{ .operator = .minus },
+                //'!' => if (self.readMatchingChar('=')) |_| .{ .operator = .ne } else .{ .operator = .bang },
+                '=' => if (self.readMatchingChar('=')) |_| .{ .operator = .eq } else .{ .operator = .assign },
+                '+' => .{ .operator = .plus },
+                '*' => .{ .operator = .asterisk },
+                '<' => .{ .operator = .lt },
+                '>' => .{ .operator = .gt },
+
+                // separators
+                ';' => .semicolon,
+                ',' => .comma,
+                '.' => .dot,
+                '{' => .lbrace,
+                '}' => .rbrace,
+                '(' => .lparen,
+                ')' => .rparen,
+                '[' => .lbracket,
+                ']' => .rbracket,
+
+                '0'...'9' => return .{ .integer = self.readInteger() },
+
+                'a'...'z', 'A'...'Z', '_' => {
+                    const v = self.readIdentifier();
+                    return if (Keywords.get(v)) |kw| .{ .keyword = kw } else .{ .identifier = v };
+                },
+
+                else => return .{ .illegal = c },
+            };
+        }
+
+        return null;
+    }
+
+    /// Returns the next non-whitespace character or null when the end of the input is reached.
+    /// i is incremented to match the index of the _next_ character.
+    fn readChar(self: *Self) ?u8 {
+        while (true) {
+            if (self.i >= self.input.len) {
+                return null;
+            }
+
+            const char = self.input[self.i];
+            self.i = self.i + 1;
+
+            if (std.ascii.isWhitespace(char)) continue;
+
+            return char;
+        }
+    }
+
+    fn peekChar(self: *Self) ?u8 {
+        if (self.i >= self.input.len) {
+            return null;
+        }
+
+        return self.input[self.i];
+    }
+
+    /// Returns the next character (and increments the index) if it matches the given character.
+    fn readMatchingChar(self: *Self, c: u8) ?u8 {
+        if (self.peekChar() == c) {
+            self.i = self.i + 1;
+            return c;
+        } else {
+            return null;
+        }
+    }
+
+    fn readCommentLine(self: *@This()) []const u8 {
+        return self.readMatching(isNoLinebeak);
+    }
+
+    fn readIdentifier(self: *@This()) []const u8 {
+        return self.readMatching(isIdentifierChar);
+    }
+
+    fn readInteger(self: *@This()) []const u8 {
+        return self.readMatching(std.ascii.isDigit);
+    }
+
+    fn readMatching(self: *Self, m: anytype) []const u8 {
+        const offset = self.i - 1;
+
+        while (if (self.peekChar()) |c| m(c) else false) {
+            self.i = self.i + 1;
+        }
+
+        return self.input[offset..self.i];
+    }
+
+    fn isIdentifierChar(c: ?u8) bool {
+        return if (c) |v| std.ascii.isAlphanumeric(v) or v == '_' else false;
+    }
+
+    fn isNoLinebeak(c: ?u8) bool {
+        return (c != '\n' and c != '\r');
+    }
+};
+
+pub const Token = union(enum) {
+    eof,
+    illegal: u8,
+    comment: []const u8,
+    identifier: []const u8,
+    integer: []const u8,
+
+    operator: Operator,
+    keyword: Keyword,
+
+    // separators
+    semicolon,
+    comma,
+    dot,
+    lbrace,
+    rbrace,
+    lparen,
+    rparen,
+    lbracket,
+    rbracket,
+
+    pub fn format(value: @This(), comptime _: []const u8, _: std.fmt.FormatOptions, writer: anytype) !void {
+        return switch (value) {
+            .keyword => writer.print("<keyword:{s}>", .{@tagName(value.keyword)}),
+            .identifier => writer.print("<symbol:{s}>", .{value.identifier}),
+            .operator => writer.print("<op:{s}>", .{@tagName(value.operator)}),
+            else => writer.print("<{s}>", .{@tagName(value)}),
+        };
+    }
+};
+
+pub const Keyword = enum {
+    create,
+    table,
+    bigint,
+    primary,
+    key,
+    autoincrement,
+    text,
+    integer,
+    select,
+    from,
+    count,
+};
+
+const Keywords = std.ComptimeStringMapWithEql(Keyword, .{
+    .{ "create", .create },
+    .{ "table", .table },
+    .{ "bigint", .bigint },
+    .{ "primary", .primary },
+    .{ "key", .key },
+    .{ "autoincrement", .autoincrement },
+    .{ "text", .text },
+    .{ "integer", .integer },
+    .{ "select", .select },
+    .{ "from", .from },
+    .{ "count", .count },
+}, std.ascii.eqlIgnoreCase);
+
+/// Operators, sorted by their precedence.
+pub const Operator = enum {
+    bang,
+    fslash,
+    assign,
+    lt,
+    gt,
+    eq,
+    ne,
+    plus,
+    minus,
+    asterisk,
+};
---
+++ src/sql/Parser.zig
@@ -0,0 +1,222 @@
+const std = @import("std");
+const Lexer = @import("./Lexer.zig");
+const TokenReader = @import("./TokenReader.zig");
+
+const Self = @This();
+
+pub fn parse(sql: []const u8, allocator: std.mem.Allocator) !SqlStatement {
+    var it = Lexer.get_tokens(sql);
+    var tr = TokenReader.init(&it);
+
+    while (tr.current) |t| {
+        switch (t) {
+            .keyword => |kw| {
+                return switch (kw) {
+                    .create => try parseCreateTable(&tr, allocator),
+                    .select => try parseSelect(&tr, allocator),
+                    else => unreachable,
+                };
+            },
+            .eof => break,
+            else => unreachable,
+        }
+    }
+
+    unreachable;
+}
+
+fn parseCreateTable(tr: *TokenReader, allocator: std.mem.Allocator) !SqlStatement {
+    _ = tr.advance(); // create -> table
+    _ = tr.advance(); // table -> name
+
+    const name = tr.current.?.identifier;
+
+    _ = tr.advance(); // name -> '('
+
+    return .{
+        .create_table = .{
+            .name = name,
+            .fields = try parseFields(tr, allocator),
+        },
+    };
+}
+
+fn parseSelect(tr: *TokenReader, allocator: std.mem.Allocator) !SqlStatement {
+    var select = SqlStatement.Select{ .fields = std.ArrayList([]const u8).init(allocator), .from = "" };
+
+    // first iteration goes from select to first field
+    while (tr.advance()) |t| {
+        switch (t) {
+            .identifier => |name| (try select.fields.addOne()).* = try allocator.dupe(u8, name),
+            .comma => {}, // skip
+            .keyword => |kw| {
+                switch (kw) {
+                    .count => {
+                        _ = tr.advance(); // (
+
+                        select.count = "*";
+                        _ = tr.advance();
+
+                        _ = tr.advance(); // )
+                    },
+                    .from => {
+                        // assume "from"
+                        const id = tr.advance().?;
+                        select.from = id.identifier;
+                        break; // no join support for now
+                    },
+                    else => unreachable,
+                }
+            },
+            else => unreachable,
+        }
+    }
+
+    return .{ .select = select };
+}
+
+fn parseFields(tr: *TokenReader, allocator: std.mem.Allocator) !std.StringHashMap(SqlStatement.Field) {
+    var fields = std.StringHashMap(SqlStatement.Field).init(allocator);
+
+    // ( <- this is where we currently are...
+    //     f1 t1 opt opt, <- these fields need to be parsed
+    //     f2 t2
+    //     PK, FK, ... <- these are not supported for now
+    // )
+
+    while (tr.advance()) |t| {
+        if (t == .rparen) {
+            break;
+        }
+
+        switch (t) {
+            .identifier => |id| try fields.put(id, try parseFieldDefinition(tr, fields.count(), allocator)),
+            // TODO: parse PK, FK, etc.: .keyword => |kw| ...
+            else => unreachable,
+        }
+    }
+
+    return fields;
+}
+
+/// Assumes tr.current is field name, consumes the comma, if any, does not consume the rparen after the last field definition
+fn parseFieldDefinition(tr: *TokenReader, index: u32, _: std.mem.Allocator) !SqlStatement.Field {
+    var field = SqlStatement.Field{
+        .index = index,
+        .typ = SqlStatement.FieldType.text,
+    };
+
+    while (tr.advance()) |t| {
+        switch (t) {
+            .keyword => |kw| switch (kw) {
+                .text => field.typ = SqlStatement.FieldType.text,
+                .integer => field.typ = SqlStatement.FieldType.int,
+                .autoincrement => field.autoincrement = true,
+                .primary => {
+                    _ = tr.advance(); // consume `key`
+                    field.pk = true;
+                },
+                else => unreachable,
+            },
+
+            else => unreachable,
+        }
+
+        if (tr.next) |next| {
+            if (next == .comma) {
+                _ = tr.advance();
+                return field;
+            }
+            if (next == .rparen) {
+                return field;
+            }
+        }
+    }
+
+    return field;
+}
+
+pub const SqlStatement = union(enum) {
+    create_table: CreateTable,
+    select: Select,
+
+    pub const Insert = struct {};
+
+    pub const CreateTable = struct {
+        name: []const u8,
+        fields: std.StringHashMap(Field),
+    };
+
+    pub const Field = struct {
+        index: u32,
+        typ: FieldType,
+        autoincrement: bool = false,
+        pk: bool = false,
+    };
+
+    pub const FieldType = enum {
+        int,
+        text,
+    };
+
+    pub const Select = struct {
+        fields: std.ArrayList([]const u8),
+        from: []const u8,
+        count: ?[]const u8 = null,
+    };
+};
+
+test "parse CREATE TABLE" {
+    const sql =
+        \\CREATE TABLE oranges
+        \\(
+        \\  id integer primary key autoincrement,
+        \\  name text,
+        \\  description text
+        \\);;
+    ;
+    var arena = std.heap.ArenaAllocator.init(std.testing.allocator);
+    defer arena.deinit();
+
+    const res = try Self.parse(sql, arena.allocator());
+
+    try std.testing.expect(res.create_table.fields.count() == 3);
+    try std.testing.expect(res.create_table.fields.get("name").?.index == 1);
+}
+
+test "SELECT name FROM apples" {
+    const sql = "SELECT name FROM apples";
+    var arena = std.heap.ArenaAllocator.init(std.testing.allocator);
+    defer arena.deinit();
+
+    const res = try Self.parse(sql, arena.allocator());
+
+    try std.testing.expect(res.select.fields.items.len == 1);
+    try std.testing.expectEqualStrings("name", res.select.fields.items[0]);
+    try std.testing.expectEqualStrings("apples", res.select.from);
+}
+
+test "SELECT f1, f2 FROM tbl" {
+    const sql = "SELECT f1, f2 FROM tbl";
+    var arena = std.heap.ArenaAllocator.init(std.testing.allocator);
+    defer arena.deinit();
+
+    const res = try Self.parse(sql, arena.allocator());
+
+    try std.testing.expect(res.select.fields.items.len == 2);
+    try std.testing.expectEqualStrings("f1", res.select.fields.items[0]);
+    try std.testing.expectEqualStrings("f2", res.select.fields.items[1]);
+    try std.testing.expectEqualStrings("tbl", res.select.from);
+}
+
+test "SELECT COUNT(*) FROM tbl" {
+    const sql = "SELECT COUNT(*) FROM tbl";
+    var arena = std.heap.ArenaAllocator.init(std.testing.allocator);
+    defer arena.deinit();
+
+    const res = try Self.parse(sql, arena.allocator());
+
+    try std.testing.expect(res.select.fields.items.len == 0);
+    try std.testing.expectEqualStrings("*", res.select.count.?);
+    try std.testing.expectEqualStrings("tbl", res.select.from);
+}
